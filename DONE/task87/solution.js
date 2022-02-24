17b38c91dc7bef90c07f86be37dd9bf35ae09ba8
Pankaj Kulkarni <kulkarnipanku@gmail.com>
2022-02-24 18:53:01 +0530

    Initial commit after 17 excercises.

---
+++ DONE/task87/solution.js
@@ -0,0 +1,11 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+
+const pi = 3.15;
+const myName = 'Pankaj';
+const yeafOfBirth = 1978;
+const gender = 'Male';
+const speedOfLight = 670616629;
---
+++ DONE/task90/solution.js
@@ -0,0 +1,5 @@
+
+import {API_URL, MAX_THREADS} from "./constants.js"
+
+console.log(MAX_THREADS);
+console.log(API_URL);
